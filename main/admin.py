860db7ae8331fb0ed4ce865a126a2bac51816153
Bibek <reaperpsych@gmail.com>
2023-11-30 21:01:45 +0545

    configured admin

--- main/admin.py
+++ main/admin.py
@@ -4,7 +4,12 @@ from main.models import Information
 # Register your models here.
 
 
-@admin.register(Information)
-class InformationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'age', 'email', 'password')
+# @admin.register(Information)
+# class InformationAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'age', 'email', 'password')
     
+
+class InformationAdmin(admin.ModelAdmin):
+    list_display=['name', 'age', 'email']
+
+admin.site.register(Information, InformationAdmin)
